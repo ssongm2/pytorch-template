2c46ca58baf255d4b3822be17921e5008f3aaa5c
ssongm2 <157574142+ssongm2@users.noreply.github.com>
2024-11-19 17:14:14 +0900

    Update new_project.py

--- new_project.py
+++ new_project.py
@@ -2,10 +2,10 @@ import sys
 from pathlib import Path
 from shutil import copytree, ignore_patterns
 
+#template file로 새로운 pytorch project 초기화하는 sciprt
+# 새로운 프로젝트 생성하려면 다음을 실행 `python3 new_project.py ../MyNewProject`
+# MyNewProject가 만들어짐
 
-# This script initializes new pytorch project with the template files.
-# Run `python3 new_project.py ../MyNewProject` then new project named 
-# MyNewProject will be made
 current_dir = Path()
 assert (current_dir / 'new_project.py').is_file(), 'Script should be executed in the pytorch-template directory'
 assert len(sys.argv) == 2, 'Specify a name for the new project. Example: python3 new_project.py MyNewProject'
